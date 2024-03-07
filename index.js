1acfe1f791821c2b6430552e2ab3fe06be971b16
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-03-07 11:27:00 +0200

    updates

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 console.clear();
 
 import express from 'express';
+import { PageHome } from './pages/PageHome.js';
+import { Page404 } from './pages/Page404.js';
+import { PageAbout } from './pages/PageAbout.js';
+import { PageServicesList } from './pages/PageServicesList.js';
+import { PageServiceInner } from './pages/PageServiceInner.js';
+
 const app = express();
 const port = 4811;
 
-app.get('/', (req, res) => {
-  res.send(homePage());
-});
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.send(aboutPage());
+    const page = new PageHome();
+    res.send(page.render());
+});
+
+app.get('/about', (req, res) => {
+    const page = new PageAbout();
+    res.send(page.render());
+});
+
+app.get('/services', (req, res) => {
+    const page = new PageServicesList();
+    res.send(page.render());
+});
+
+app.get('/services/:serviceId', (req, res) => {
+    const page = new PageServiceInner(req.params);
+    res.send(page.render());
+});
+
+app.use((req, res, next) => {
+    const page = new Page404();
+    res.status(404).send(page.render());
 });
 
 app.listen(port, () => {
-  console.log(`URL: http://localhost:${port}`);
+    console.log(`URL: http://localhost:${port}`);
 });
